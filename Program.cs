2cc2b44f9b50eb85acc4a53d6d6a317450b3e861
JayanthNarla <47258705+JayanthNarla@users.noreply.github.com>
2020-12-16 16:38:00 +0530

    timepass

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Question
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            int st, en, cnt = 0;
+
+            Console.WriteLine("Num 1:-");
+
+            st = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Num 2:-");
+
+            en = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < en; i++)
+
+            {
+
+                for (int j = 1; j < en; j++)
+
+                {
+
+                    if (i % j == 0)
+                    {
+                        cnt++;
+                    }
+
+                }
+
+                if (cnt == 2)
+
+                {
+
+                    //Write Print statement here
+
+                }
+
+                cnt = 0;
+
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
